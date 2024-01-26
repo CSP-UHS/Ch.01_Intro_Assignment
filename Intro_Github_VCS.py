7fb879653bc95a9a61ef49b9aa097e93fd93f7d7
Marc Hermon <hermonm@urbandaleschools.com>
2024-01-26 07:54:26 -0600

    Update Intro_Github_VCS.py

--- Intro_Github_VCS.py
+++ Intro_Github_VCS.py
@@ -17,138 +17,117 @@ Tell me some interesting facts about yourself.
 
 
 
-----------------Elias Bethards----------------
-this is a testing branch!
-CSP is a class
-Mr. Hermon is a teacher
-It is a Wednesday
-I play soccer
-I have exactly 7 mugs in the backseat of my car
-On average, more people die each year to vending machines than to sharks
-There are more possible iterations of a game of chess than there are atoms in the observable universe
-the diameter of the sun is approx. 1.4 million km (about 870,000 mi)
-------------------amal choudhury------------------
-my faavotire color is teal
-i like golf
-i love tennis
-i have 6 children ages 2-47
-i am 16
-i have 6 grandchildren
-i own 7 better kayaks
-I like camping more
-i work at brio of johnston
-------------------Treajan Diersen------------------
-this is a testing branch
-Tom Clancy's Rainbow Six Siege is a video game.
-My dog is cool.
-Water is essential.
-Food is alright.
-only good food tho.
-Blue is a cool color.
-I like to leave school.
-My bed is comfortable.
-------------------Muhammad Elshafie------------------
-My favorite color is green
-I love Python
-I love Soccer
-I have a family members of 6
-I love Video games
-I love travelling a lot
-I love Watching movies
-I like Some shows
-I cant wait till I Graduate
-------------------Truman Folkers------------------
-I have one dog named Bailey.
-I like playing basketball and tennis.
-I like playing drums for jazz band, show band, and marching band.
-My favorite music artist is Boz Scaggs.
-My favorite color is blue.
-I lived in Cleveland, Ohio for most of my childhood.
-I work at Jimmy Johns.
-My favorite type of food is Asian food.
-My favorite place to vacation is Clearwater, Florida.
-------------------Carlos Guzman------------------
-My favorite color is yellow
-I have a dog
-I have seven siblings
-I'm in choir
-I'm in Cross-Country
-I'm in track
-I like fishing
-Summer is my favorite season
-I'm learning guitar
-------------------Jackson Krier------------------
-My favorite color is blue
-I dislike english class
-I have played Hockey for 10 years
-I have a brother in college at UNI
-I like lifting
-I plan on going to ASU
-I want to study comp sci in college
-I like traveling
-Friday is the best day of the week
-------------------Ava L------------------
-My favorite Color is Blue
-I have two dogs
-I work for the school in the summer
-I have a sister in Collage
-I like art
-Im in theater
-I like to read
-I like coding
-Im thinking of getting a BA in Computer Science
-------------------Jace Leahy------------------
-this is a testing branch
-1
-2
-3
-4
-5
-6
-7
-8
-------------------Caleb Little------------------
-My favorite color is purple
-I love cycling & walking
-I play the flute
-Coding is fun, I love python
-My favorite classes are csp and apush
-I have two cats
-I have two siblings
-I am the youngest in my family
-I love learning
-------------------Walker Lowe------------------
-I work at Fareway
-My favorite color is blue
-I like pirates
-I like spending time with my friends
-I like cars
-I drive a WRX
-My favorite subject is History
-I have three pets
-I played baseball for 12 years
+------------------Student 02------------------
+
+
+
+
+
+
+
+
+
+------------------Student 03------------------
+
+
+
+
+
+
+
+
+
+------------------Student 04------------------
+
+
+
+
+
+
+
+
+
+------------------Student 05------------------
+
+
+
+
+
+
+
+
+
+------------------Student 06------------------
+
+
+
+
+
+
+
+
+
+------------------Student 07------------------
+
+
+
+
+
+
+
+
+
+------------------Student 08------------------
+
+
+
+
+
+
+
+
+
+------------------Student 09------------------
+
+
+
+
+
+
+
+
+
+------------------Student 10------------------
+
+
+
+
+
+
+
+
+
+------------------Student 11------------------
+
+
+
+
+
+
+
+
+
+------------------Student 12------------------
+
+
+
+
+
+
+
+
+
 ------------------Student 13------------------
-My name is Cung Mang
-I like to play soccer
-I go to UHS
-I am coding in Python
-I am in CSP
-I like the school lunch
-My CSP's teacher name is Hermon
-I like CSP
-I am in 8 classes right now in UHS.
-------------------Logan Mills------------------
-Favorite color is dark green
-I'm into cybersecurity / ethical hacking
-I work on bug bounties in my free time
-I love vintage tech (I ordered an early 2000's ex German Military laptop)
-I own my own custom keyboard that I built
-I prefer linux (Specifically Kali)
-I run my own SIEM server at home
-I work at Pancheros
-I own a 94 Mustang GT
-------------------Thomas Mitchell------------------
-hi
 
 
 
@@ -157,156 +136,173 @@ hi
 
 
 
-------------------Gavin------------------
-I am senior this year
-My favorite food is apple
-I am a part-time member of CORE Club
-I participate in Boys XC & Track
-I work at Hy-vee
-I have a dog, her name is Foxy
-My favorite music artist is Isaac Dunbar
-Some of my hobbies are gaming, art, and cooking
-I love snacks (they're in my backpack)
-------------------THINH NGO------------------
-my favorite color is blue
-working out is fun
-school is kind of boring right now
-i work a lot
-money is good
-i like eating steak
-i like eating salmon
-i cook
-money
-------------------Tommy Ngo------------------
-My favorite color is blue
-I used to work at Olive Garden
-I'm a senior
-My favorite food is tacos
-I play Overwatch
-I'm in Studio
-I like cats
-My favorite subject is history
-My Zenko Tea order is fresh mango milk with lychee jelly
--------------Aidan Ordagic------------------
-my fav color is gold
-I like playing star wars battlefront 2
-Im 15
-My fav soccer player is Modric
-my fav UFC fighter is Jon Jones
-my favorite food is mac and cheese
-my favorite restaurant is Charlies at jordan creek
-Favorite holiday is Christmas
-favorite season is winter
-------------------Blayne Pettengill------------------
-my favorite color is Black
-I have been playing alot of terraria recently
-I love DnD
-I've Been enjoying watching Psych
-My favorite food is Pizza
-I enjoy coding
-I recently bought my own computer
-I like puppy chow
-I have a dog and cat
-------------------Jonathan Reischel------------------
-My favorite color is purple
-I have a husky shepard dog mix called Sampson
-I work as a rides operator at Adventure land
-My favorite sports team is the Minnesota Vikings
-I like to fish alot
-I am on the Bowling team at the high school
-My favorite song is Purple rain by Prince
-I like to code
-I have 1 brother
-------------------AJ Reisinger------------------
-I like the Minnesota Vikings
-I am a big American Football fan
-I have 1 dog at home
-Christmas is my favorite holiday
-Winter is my favorite season
-My favorite UFC fighter is khabib
-My favorite NFL player right now is Adam Thielen
-Berserk is my favorite fictional series
-I play video games in my free time
-------------------Jake Staton------------------
-I work at the the Clive Aquatics Center
-I have a dog and three cats (Magnus, Tyrone, Toonces and Tuco)
-I have no children
-I play guitar
-My favorite foods are eggs and mac n cheese
-I live in Urbandale
-I was born in Iowa
-I have two brothers
-I am a senior at Urbandale High School
-------------------Alex Tiemens------------------
-My favorite color is green
-I have 2 brothers
-I have 2 Dogs
-I have 2 cats
-I've had 5 cats
-I've had 3 dogs
-I am 16
-I like fishing
-I am 5,8
-------------------Hai Tran------------------
-I like kiwis.
-I like cherries.
-I like strawberries.
-I like grapes.
-I like watermelon.
-I like pineapple.
-I like blueberries.
-I like mangoes.
-I like oranges.
-------------------Keira VanderLeest------------------
-I have 3 cats and a dog
-I like to draw
-I play in band
-I am the assistant lighting designer for theater
-I placed within the top 100 in an online rhythm game
-I have one older sister
-I like reading
-My favorite colour is blue
-I play 3 instruments
-------------------Stephen Walters------------------
-My favorite class is CSP
-My favorite color is green
-I play soccer
-I like coding
-I have two older brothers
-I have two dogs
-I haven't won any candy yet
-I have a car
-I am in showchoir
-------------------Jerry Zheng------------------
-my favorite color is blue
-I like to code
-I like to travel
-My favorite food is Lobster
-I enjoy math
-I am the only child in my family
-My birthday is 9/23
-I enjoy swimming
-I hope U Hub crashes so chaos happens
-------------------Caden McLaughlin------------------
-I don't have that much that's interesting about me
-So these lines are mostly filler because I couldn't think of anything else
-I share a birthday with Abraham Lincoln
-I can put both legs behind my head
-I can turn my head almost completely around
-I have a genetic disorder called Ehlers-Danlos Syndrome
-I've still never broken a bone
-That's about everything that's interesting about me
-These lines are also filler but with them I have filled all the lines
-------------------Mr. Hermon------------------
-My favorite color is blue
-I like fishing
-I love coding
-I have four children ages 13-30
-I have 3 grandchildren
-I like camping
-I own 7 kayaks
-My job is teaching the future of America
-If U-Hub crashes, it's not because of bad code!
+
+------------------Student 14------------------
+
+
+
+
+
+
+
+
+
+------------------Student 15------------------
+
+
+
+
+
+
+
+
+
+------------------Student 16------------------
+
+
+
+
+
+
+
+
+
+------------------Student 17------------------
+
+
+
+
+
+
+
+
+
+------------------Student 18------------------
+
+
+
+
+
+
+
+
+
+------------------Student 19------------------
+
+
+
+
+
+
+
+
+
+------------------Student 20------------------
+
+
+
+
+
+
+
+
+
+------------------Student 21------------------
+
+
+
+
+
+
+
+
+
+------------------Student 22------------------
+
+
+
+
+
+
+
+
+
+------------------Student 23------------------
+
+
+
+
+
+
+
+
+
+------------------Student 24------------------
+
+
+
+
+
+
+
+
+
+------------------Student 25------------------
+
+
+
+
+
+
+
+
+
+------------------Student 26------------------
+
+
+
+
+
+
+
+
+
+------------------Student 27------------------
+
+
+
+
+
+
+
+
+
+------------------Student 28------------------
+
+
+
+
+
+
+
+
+
+------------------Student 29------------------
+
+
+
+
+
+
+
+
+
+------------------Student 30------------------
+
+
+
+
+
 
 
 
