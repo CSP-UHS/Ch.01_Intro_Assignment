95f52ed9feee2ad8110eef73874c4762e63cbdfd
fishernoa <fishernoa@urbandaleschoosl.com>
2024-09-10 14:23:53 -0500

    I'm committed to Careers and Entrepreneurship!

--- Intro_Github_VCS.py
+++ Intro_Github_VCS.py
@@ -199,7 +199,7 @@ Tell me some interesting facts about yourself.
 
 ------------------Student 20 Noah Fisher------------------
 My favorite color is amaranth!
-
+My favorite is Careers and Entrepreneurship!
 
 
 
