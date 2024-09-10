24111269d55728a20816a6050accac56383b37b6
reecetho <reecetho@urbandaleschools.com>
2024-09-10 14:23:35 -0500

    I'm commited to CSP

--- Intro_Github_VCS.py
+++ Intro_Github_VCS.py
@@ -149,7 +149,7 @@ Tell me some interesting facts about yourself.
 
 ------------------Student 15 Thomas Reece------------------
 My favorite color is Purple
-
+My favorite class is Cybersecurity
 
 
 
