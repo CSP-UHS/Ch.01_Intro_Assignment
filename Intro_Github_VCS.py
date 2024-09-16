92f6536cdec69da73145b6c551d1963a2e4516cf
Marc Hermon <hermonm@urbandaleschools.com>
2024-09-16 13:21:39 -0500

    Update Intro_Github_VCS.py
    reset for 2026

--- Intro_Github_VCS.py
+++ Intro_Github_VCS.py
@@ -7,26 +7,26 @@ Tell me some interesting facts about yourself.
 
 
 
-------------------Student 01: Kaylen Morris------------------
-My favorite color is blue
-I owned a pet bunny when I was younger
-I am a junior at Urbandale High School
-I have blue eyes
-My favorite movie of all time is Tangled
-I have four dogs and one cat
-I have a sister who is a freshman at UHS and a brother who is a 6th grader at UMS
-I am double-jointed in my wrists
-I am 5'7
-------------------Student 02: Blayne Pettengill------------------
-My favorite color is Black
-Some of my favorite games are Skyrim, Terraria, and Outer Wilds
-I love DnD
-I've Been enjoying watching Psych
-My favorite food is Pizza
-I enjoy coding
-My favorite cookies is snickerdooldes
-I have one brother
-I have a dog and 2 cats
+------------------Student 01------------------
+
+
+
+
+
+
+
+
+
+------------------Student 02------------------
+
+
+
+
+
+
+
+
+
 ------------------Student 03------------------
 
 
@@ -37,103 +37,103 @@ I have a dog and 2 cats
 
 
 
-------------------Noah Smit This is the Master Branch------------------
-I play video games
-I have 3 cats
-My cats' names are Rory, Callie, and Clyde
-My favorite movie is Into the Spider-verse
-My favorite food is spaghetti
-My dog's name is Libby
-I work at Hy-vee
-I plan to go to Iowa State
-I want to go to college for Engineering
-------------------Student 05: Hudson Kerchal------------------
-My favorite color is purple
-I've been to mexico 12 times
-I have 1 sister
-I like to play golf and play video games
-I used to play 6 sports
-I have lived in Iowa my whole life
-My favorite class is Science
-I like listening ot music
-My favorite music artist is MF DOOM
-------------------Student 06:Ian Gage------------------
-my favorite color is red
-I am a butcher a hyvee
-I play the euphonium in our band, and have done so for 7 years
-I am a fan of the band Jamiroquai
-I enjoy wordle
-I had a Dog
-Kitkats are my favorite candy bar
-I am the Section leader of the Euphonium section
-I have 1 sister
-------------------Student 07: Elaina Klaas------------------
-My favorite color is purple
-I am left handed
-I am a junior at Urbandale High School
-I play volleyball and I bowl
-I work at Lifetime Fitness
-I am 5'2 
-I have 2 dogs and 2 cats
-I have a brother who is a freshman in college 
-I want to be a computer scientist 
-------------------Student 08: Austin Weir------------------
-I like to read
-My favorite color is green
-I like Spanish
-I am in the play
-I sometimes play D&D
-I like to play games
-I love math
-I eat food
-I drink water
-------------------Student 09: Alex Ballard------------------
-My favorite color is green.
-I own a wet floor sign.
-I have 4 cats, 2 dogs and 4 geckos.
-I have younger brother and sister.
-I am from Minnesota.
-I have been in band and in orchestra for 5 years each.
-Played soccer for almost half my life.
-My grandparents make homemade maple syrup.
-I enjoy having a science class.
-------------------Student 10 - Cayden Feldotto------------------
-My favorite color is Aqua
-I am a big fan of Kanye West's music
-I am a sophomore
-I have blue eyes
-I have a twin
-I work at a grocery store
-My favorite hobby is playing video games
-My favorite video game is Elden Ring
-I have a bulldog named Rosie
-------------------Student 11 Hunter Clark------------------
-I like green
-My favorite class I'm taking is Chemistry
-I am on a swim team
-I own a cat
-My favorite book series is Percy Jackson
-I love science of any kind
-I am considered the house tech support agent.
-I know how to work with electronic hardware
-I play D&D!
-------------------Student 12 Markoda Mann------------------
-My favorite color is purple!
-My favorite class this year is Cubersecurity >:)
-My favorite game is Goat Simulator III
-I have four toes (not really) (totally)
-I am going to tell some of my best half jokes now.
-I once knew a woman with a taser.
-Don't sleep with alligators.
-inflatable bloons are domestic terrorists.
-thank you for your ears.
-------------------Student 13 Simon Woida------------------
-I don't have a favorite color
-ELP
-I can't stand people chewing with their mouth open next to me
-I don't know how to ride a bike
-I've never broken a bone
-I don't have anything else to put here
+------------------Student 04------------------
+
+
+
+
+
+
+
+
+
+------------------Student 05------------------
+
+
+
+
+
+
+
+
+
+------------------Student 06------------------
+
+
+
+
+
+
+
+
+
+------------------Student 07------------------
+
+
+
+
+
+
+
+
+
+------------------Student 08------------------
+
+
+
+
+
+
+
+
+
+------------------Student 09------------------
+
+
+
+
+
+
+
+
+
+------------------Student 10------------------
+
+
+
+
+
+
+
+
+
+------------------Student 11------------------
+
+
+
+
+
+
+
+
+
+------------------Student 12------------------
+
+
+
+
+
+
+
+
+
+------------------Student 13------------------
+
+
+
+
+
+
 
 
 
@@ -147,126 +147,126 @@ I don't have anything else to put here
 
 
 
-------------------Student 15 Thomas Reece------------------
-My favorite color is Purple
-My favorite class is Cybersecurity
-I'm a cat person
-I'm not in any sports
-I'm in CSP
-I don't have any pets
-A show I watched as a child is Phinneas and Ferb
-I went on vaction to Washington DC in 4th grade
-I like coding
-------------------Student_16_Seth_Cain------------------ Lines 160-169
-My favorite color is Royal Purple!
-Favorite class is a tie between CSP and Psychology.
-The fastest I've solved a Rubik's Cube in 26 Seconds.
-I have 4 Sisters.
-I only have a single brother.
-I main Wolf in SSBU.
-I've played/beaten Persona 5 Royal.
-I have very stretchy skin.
-I'm in the middle of writing my book.
-------------------Student 17 Areeb Ahmed------------------
-My favorite color is red!
-My favorite class is japanese, CSP and English
-I came from other country name Bangladesh.
-I had eight kittens and 3 cats which was really fun and little bit of troublesome
-If i talk about my country and USA then I will say USA is better than Bangladesh.
-My country is still growing, we had a big fight on july and augest 5th was the independece for the students and the citizen
-I am mostly like a cat person.
-Last year was my first time watching snow and it was really fun
-I didn't catch cold, fever or anything so thats record for me.I love coding and wants to be a pogrammer or a developer.
-------------------Student 18: Precious Karyee------------------
-my favorite color is navy
-2nd fav color is olive green
-3rd favorite color is pink
-4th fav is forest green
-my favorite movie is the greatest showman
-my second favorite movie is black panther
-I run track
-I have 5 siblings
-my favorite thing to do is sleep
-------------------Student 19 Medin Burnic------------------
-My favorite color is Red!
-My favorite class I'm taking this year is AP Euro!
-I love History & Social Studies!
-I love playing games with my friends!
-This summer was great! I'm looking forward to next summer!
-I have a twin sister!
-I'm going to be hosting a German student next month for UHS!
-I don't have pets, but I like cats!
-I deeply appreciate and value all my teachers!
-------------------Student 20 Noah Fisher------------------
-My favorite color is amaranth!
-My favorite is Careers and Entrepreneurship!
-I race Go-Karts
-I am preparing to join autocross racing next year
-I am working towards having my competition race licence within the next 2 years
-I am going to go to a racing school over in chicago within the next few years
-I am ranked top 11,000 out of 18 million people across the world for go-karting right now
-I am going to be an apprentice for a mechanic in 2025
-I own and produce my own cologne brand
-------------------Student 21 Nate Powers------------------
-My favorite color is red!
-My favorite class is CSP
-I run cross country and track
-I plan to go to Iowa State
-I would like to be an AI scientist
-I have red hair
-I played soccer for 10 years
-I am familiar with HTML, CSS, Java, CSS, and Python
-I drive a truck that is older than me
-------------------Student 22 Logan Knisley------------------
-My favorite color/shade is seafoam green
-My favorite class so far is AP Chemistry
-I have a dog, Grace, and a cat, Frenchie
-I have a passion for learning history
-I enjoy watching soccer (Atlético Madrid) and football (Green Bay Packers)
-I like to write intriguing narratives (that don't go past chapter 1)
-I help track and visualize statistics for the Urbandale boys soccer team
-I work with Godot (GDScript/C#) to make quality* games
-I use GTK (Python/C/Rust in the near future) to customize linux desktop environments
-------------------Student 23:Dayne Rew ------------------
-My favorite color is pink!
-My favorite class is CSP!
-My favorite food is Ramen Noodles and Pizza!
-I play varisty golf and varsity wrestling!
-I love watching YouTube!
-I Make muckbangs every day!
-I'm a huge Iowa State fan!
-I have 2 fat pugs at home!
-My favorite video games are Call Of Duty and Fortnite!
-------------------Student 24 Korben Henry------------------
-my fav color is red
-Photography II
-i like fixing hardwhere on computers and phones.
+------------------Student 15------------------
 
 
 
 
 
 
-------------------Student 25: Calen Heller------------------
-My favorite color is blue
-I love cars
-I play basketball and baseball
-I have 2 sisters
-I work at Scheels
-I have a dog who is a mutt
-I like spending my time fishing
-I enjoy building legos
-I play 2K
-------------------Student 26 Devin Johnson
-Black
-my favroite class is ijag
-im black
-i hang out with friends
-i go to walmart a lot
-i like potatos
-im a size 13
-im 6'2
-korben is short
+
+
+
+------------------Student 16------------------
+
+
+
+
+
+
+
+
+
+------------------Student 17------------------
+
+
+
+
+
+
+
+
+
+------------------Student 18------------------
+
+
+
+
+
+
+
+
+
+------------------Student 19------------------
+
+
+
+
+
+
+
+
+
+------------------Student 20------------------
+
+
+
+
+
+
+
+
+
+------------------Student 21------------------
+
+
+
+
+
+
+
+
+
+------------------Student 22------------------
+
+
+
+
+
+
+
+
+
+------------------Student 23------------------
+
+
+
+
+
+
+
+
+
+------------------Student 24------------------
+
+
+
+
+
+
+
+
+
+------------------Student 25------------------
+
+
+
+
+
+
+
+
+
+------------------Student 26------------------
+
+
+
+
+
+
+
+
+
 ------------------Student 27------------------
 
 
